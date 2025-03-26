f624b38e3cb723fe5bc53ba5b3180771fde8ef57
Devansh Sain <scs1605678@wizblock.ai>
2025-03-26 21:37:54 +0530

    main

---
+++ devansh/hello_world_on_oled.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+url = "https://wizklub.com/api/secured/wiziot-poll-request/"
+payload = json.dumps({"mode": "WRITE", "device_id": "sc13503", "source": "API", "api": "oled", "type": "text", "msg_to_write": "Hello world", "x_axis": "0", "y_axis": "0"})
+
+headers = {"Api-Access-Key": "MzZyRHh6VVVjM1ZpTU5vdkxrYWU=", "Api-Secret-Key": "MU5WWmxhb1JraTJFNVpQRTd0SWg5QVFLak9RaDJ0RlJtUQ==", "Content-Type": "application/json"}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+print(response.text)
